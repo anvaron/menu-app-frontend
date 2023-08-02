cd6f91b9896e909591f435e5410328e7895eb63b
Andres Varon <anvaron@gmail.com>
2023-08-02 15:05:53 -0400

    Menu card toppings rendering updated

--- src/components/MenuCard/MenuCard.js
+++ src/components/MenuCard/MenuCard.js
@@ -3,6 +3,7 @@ import "./MenuCard.css";
 
 export default function MenuCard({ menu }) {
   const { id, name, price, image, toppings, shortDescription } = menu;
+  let tops = [];
   return (
     <div className="card" key={id}>
       <h1 className="name">{name}</h1>
@@ -13,11 +14,15 @@ export default function MenuCard({ menu }) {
       <div className="brief">{shortDescription}</div>
       <div className="toppings">
         Toppings:
-        {toppings.map((topping, index) => (
-          <span key={index}>
-            {`${(index ? " - " : " ") + topping}`}
-          </span>
-        ))}
+        {toppings.length ? (
+          toppings.map((topping, index) => (
+            <span key={index}>
+              {`${(topping ? ` ${topping} ` : "") }`}
+            </span>
+          ))
+        ) : (
+          <span>{' None '}</span>
+        )}
       </div>
       <div className="price">Price: <span>${price.toFixed(2)}</span></div>
     </div>
